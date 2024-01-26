2e617b1c840b1f248ab00967313ee9348ab7b942
Sital1 <heavywater221@gmail.com>
2024-01-25 22:30:04 -0600

    links

--- components/intro.tsx
+++ components/intro.tsx
@@ -80,7 +80,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/sital-sitoula-418780192/"
           target="_blank"
         >
           <BsLinkedin />
@@ -88,7 +88,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/Sital1"
           target="_blank"
         >
           <FaGithubSquare />
